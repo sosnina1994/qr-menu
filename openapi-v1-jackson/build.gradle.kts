98d89776ff2a18667aff0e5771f9b7248169cfd5
SosninaNV <n.sosnina1994@yandex.ru>
2023-08-16 21:41:02 +0500

    feat: add openapi.generator

---
+++ openapi-v1-jackson/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    kotlin("jvm")
+    id("org.openapi.generator")
+}
+
+dependencies {
+    val jacksonVersion: String by project
+    implementation(kotlin("stdlib"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    testImplementation(kotlin("test-junit"))
+}
+
+sourceSets {
+    main {
+        java.srcDir("$buildDir/generate-resources/main/src/main/kotlin")
+    }
+}
+
+/**
+ * Настройка генерации моделей
+ */
+openApiGenerate {
+    val openapiGroup = "${rootProject.group}.api.v1"
+    generatorName.set("kotlin")
+    packageName.set(openapiGroup)
+    apiPackage.set("$openapiGroup.api")
+    modelPackage.set("$openapiGroup.models")
+    invokerPackage.set("$openapiGroup.invoker")
+    inputSpec.set("$rootDir/specs/specs-dish-v1.yml")
+
+    globalProperties.apply {
+        put("models", "")
+        put("modelDocs", "false")
+    }
+
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "string",
+            "enumPropertyNaming" to "UPPERCASE",
+            "serializationLibrary" to "jackson",
+            "collectionType" to "list"
+        )
+    )
+}
+
+tasks {
+    compileKotlin {
+        dependsOn(openApiGenerate)
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,16 @@
 rootProject.name = "qr-menu"
 
 pluginManagement {
+    val kotlinVersion: String by settings
+    val openapiVersion: String by settings
+
     plugins {
-        val kotlinVersion: String by settings
         kotlin("jvm") version kotlinVersion apply false
+        kotlin("plugin.serialization") version kotlinVersion apply false
+
+        id("org.openapi.generator") version openapiVersion apply false
     }
 }
 
 include("quickstart")
+include("openapi-v1-jackson")
