ac565155eb8beb1e4040075d4171c159dfe972a6
SosninaNV <n.sosnina1994@yandex.ru>
2023-09-29 18:26:26 +0500

    feat: add kafka

---
+++ qr-menu-app-kafka/src/main/kotlin/ru/sosninanv/qrmenu/app/kafka/AppKafkaConfig.kt
@@ -0,0 +1,20 @@
+package ru.sosninanv.qrmenu.app.kafka
+
+class AppKafkaConfig(
+    val kafkaHosts: List<String> = KAFKA_HOSTS,
+    val kafkaGroupId: String = KAFKA_GROUP_ID,
+    val kafkaTopicInV1: String = KAFKA_TOPIC_IN_V1,
+    val kafkaTopicOutV1: String = KAFKA_TOPIC_OUT_V1,
+) {
+    companion object {
+        const val KAFKA_HOST_VAR = "KAFKA_HOSTS"
+        const val KAFKA_TOPIC_IN_V1_VAR = "KAFKA_TOPIC_IN_V1"
+        const val KAFKA_TOPIC_OUT_V1_VAR = "KAFKA_TOPIC_OUT_V1"
+        const val KAFKA_GROUP_ID_VAR = "KAFKA_GROUP_ID"
+
+        val KAFKA_HOSTS by lazy { (System.getenv(KAFKA_HOST_VAR) ?: "").split("\\s*[,;]\\s*") }
+        val KAFKA_GROUP_ID by lazy { System.getenv(KAFKA_GROUP_ID_VAR) ?: "qr-menu" }
+        val KAFKA_TOPIC_IN_V1 by lazy { System.getenv(KAFKA_TOPIC_IN_V1_VAR) ?: "qr-menu-in-v1" }
+        val KAFKA_TOPIC_OUT_V1 by lazy { System.getenv(KAFKA_TOPIC_OUT_V1_VAR) ?: "qr-menu-out-v1" }
+    }
+}
---
+++ qr-menu-app-kafka/src/test/kotlin/FirstTest.kt
@@ -0,0 +1,10 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FirstTest {
+
+    @Test
+    fun firstTest() {
+        assertEquals(3, 1 + 2)
+    }
+}
